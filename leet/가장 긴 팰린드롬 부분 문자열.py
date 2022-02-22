149ac26f8eb6fc979e7779d0738abb949bab0aeb
asusikai <75575520+asusikai@users.noreply.github.com>
2022-02-22 23:24:50 +0900

    6번 2개 포인터로 해결

---
+++ leet/가장 긴 팰린드롬 부분 문자열.py
@@ -0,0 +1,43 @@
+'''
+
+양 방향 포인터 2쌍을 사용하여 체크한다
+짝수 팰린드롬, 홀수 팰린드롬 체크
+
+'''
+
+from collections import deque
+from tabnanny import check
+
+palindrome = deque()
+
+inputs = "cbbd"
+
+def checker(string, front, end):
+    
+    if front <0 or end>=len(string):
+        return
+    
+    if string[front] == string[end]:
+        if front != end:
+            palindrome.appendleft(string[front])
+            palindrome.append(string[end])
+            
+        
+        else:
+            palindrome.append(string[front])
+        
+        checker(string, front-1, end+1)
+
+final = deque()
+
+for i in range(len(inputs)):
+    checker(inputs, i, i)
+    final = final if len(final) > len(palindrome) else palindrome.copy()
+    palindrome.clear()
+    
+    checker(inputs, i, i+1)
+    final = final if len(final) > len(palindrome) else palindrome.copy()
+    palindrome.clear()
+
+print(final)
+    
